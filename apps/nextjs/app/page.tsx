984e3559250661ab28363057623c1ee297c80982
Aradhana Indra Daniswara <aradhanaindra.daniswara@wingscorp.com>
2024-08-27 16:25:00 +0700

    progress

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@repo/ui/button";
+import NextJSLogo from "@/components/nextjs-logo";
 
 export default function Home() {
-  return (
-    <>
-    <Button>Hai Indra</Button>
-    </>
-  );
+    return (
+        <main className="flex items-center justify-center min-h-screen from-neutral-800 to-neutral-900 bg-gradient-to-br">
+            <div>
+                <h1 className="text-white">This is your NextJS App</h1>
+                <NextJSLogo />
+            </div>
+        </main>
+    );
 }
